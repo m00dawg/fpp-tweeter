746fe12abd69299e0c25e67212dfda6379faad14
Tim Soderstrom <tim@moocowproductions.org>
2015-10-05 18:05:14 -0500

    Basic tweets now work, yay

---
+++ tweet.php
@@ -0,0 +1,32 @@
+<?php
+///opt/fpp/plugins/rds_plugin/callbacks --type media --data {"type":"both","Sequence":"rds test.fseq","Media":"test song.mp3","title":"Song Title","artist":"Song Artist"}
+require_once 'TwitterAPIExchange.php';
+require_once 'twitter.conf.php';
+
+const TWEET_TYPE=1;
+const TITLE_ARG=2;
+
+if(!isset($argv))
+  exit(1);
+
+if($argv[TWEET_TYPE] == 'Song')
+{
+  $message = "Now playing: ".$argv[TITLE_ARG];
+  $url = 'https://api.twitter.com/1.1/statuses/update.json';
+  $requestMethod = 'POST';
+  $postfields = array(
+    'status' => $message
+  );
+  echo $message;
+
+  //$twitter = new TwitterAPIExchange($twitterSettings);
+  //echo $twitter->buildOauth($url, $requestMethod)
+  //  ->setPostfields($postfields)
+  //  ->performRequest();
+}
+
+//$titleArgIndex = 7;
+//$title = explode(':',$argv[$titleArgIndex]);
+
+
+?>
