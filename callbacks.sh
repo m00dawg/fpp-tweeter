fe16ddff3b068c8932589d50abf8eac447566cc4
Tim Soderstrom <tim@moocowproductions.org>
2015-10-05 18:13:39 -0500

    Added more settings (Twitter, Song Change strings)

--- callbacks.sh
+++ callbacks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TITLE_ARG=$7
+
 if [ $1 == "--list" ]
 then
     echo media
@@ -9,7 +11,7 @@ if [ $1 == "--type" ]
 then
     if [ $2 == "media" ]
     then
-        echo "media stuff"
+        TITLE=`echo $TITLE_ARG | awk -F: {'print $2'}`
+        php ./tweet.php Song "$TITLE"
     fi
 fi
-
--- settings.php
+++ settings.php
@@ -1,9 +1,13 @@
 <h1>Tweeter Settings</h1>
 
-<p>Twitter Handle: <?php PrintSettingText("Account", 0, 0, 50, 50, "tweeter"); ?> <?php PrintSettingSave("Twitter Account", "Account", 0, 0, "tweeter"); ?></p>
-<p>API Key: <?php PrintSettingText("APIKey", 0, 0, 255, 64, "tweeter"); ?> <?php PrintSettingSave("API Key", "APIKey", 0, 0, "tweeter"); ?></p>
+<p>Song Change Message Prefix: <?php PrintSettingText("SongMessagePrefix", 0, 0, 64, 64, "tweeter"); ?> <?php PrintSettingSave("Song Message Prefix", "SongMessagePrefix", 0, 0, "tweeter"); ?></p>
+<p>Song Change Message Postfix: <?php PrintSettingText("SongMessagePostfix", 0, 0, 64, 64, "tweeter"); ?> <?php PrintSettingSave("Song Message Postfix", "SongMessagePostfix", 0, 0, "tweeter"); ?></p>
+
+<p>OAUTH Access Token: <?php PrintSettingText("TwitterOAUTHAccessToken", 0, 0, 64, 64, "tweeter"); ?> <?php PrintSettingSave("OAUTH Access Token", "TwitterOAUTHAccessToken", 0, 0, "tweeter"); ?></p>
+<p>OAUTH Access Token Secret: <?php PrintSettingText("TwitterOAUTHAccessSecret", 0, 0, 64, 64, "tweeter"); ?> <?php PrintSettingSave("OAUTH Access Secret", "TwitterOAUTHAccessSecret", 0, 0, "tweeter"); ?></p>
+<p>Consumer Key: <?php PrintSettingText("TwitterConsumerKey", 0, 0, 64, 64, "tweeter"); ?> <?php PrintSettingSave("Consumer Key", "TwitterConsumerKey", 0, 0, "tweeter"); ?></p>
+<p>Consumer Secret: <?php PrintSettingText("TwitterConsumerSecret", 0, 0, 64, 64, "tweeter"); ?> <?php PrintSettingSave("Consumer Key", "TwitterConsumerSecret", 0, 0, "tweeter"); ?></p>
 
 Doc: https://github.com/FalconChristmas/fpp/blob/d3d0f235257d23b74de8ebc19f2f17778b0dbb85/www/common.php
 
 <br />
-
