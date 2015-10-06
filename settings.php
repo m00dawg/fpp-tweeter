2263b6fb0cbb30ca466221c24699505c1cd1ec93
Tim Soderstrom <tim@moocowproductions.org>
2015-10-05 19:22:24 -0500

    Key vs Secret

--- settings.php
+++ settings.php
@@ -6,7 +6,7 @@
 <p>OAUTH Access Token: <?php PrintSettingText("TwitterOAUTHAccessToken", 0, 0, 64, 64, "tweeter"); ?> <?php PrintSettingSave("OAUTH Access Token", "TwitterOAUTHAccessToken", 0, 0, "tweeter"); ?></p>
 <p>OAUTH Access Token Secret: <?php PrintSettingText("TwitterOAUTHAccessSecret", 0, 0, 64, 64, "tweeter"); ?> <?php PrintSettingSave("OAUTH Access Secret", "TwitterOAUTHAccessSecret", 0, 0, "tweeter"); ?></p>
 <p>Consumer Key: <?php PrintSettingText("TwitterConsumerKey", 0, 0, 64, 64, "tweeter"); ?> <?php PrintSettingSave("Consumer Key", "TwitterConsumerKey", 0, 0, "tweeter"); ?></p>
-<p>Consumer Secret: <?php PrintSettingText("TwitterConsumerSecret", 0, 0, 64, 64, "tweeter"); ?> <?php PrintSettingSave("Consumer Key", "TwitterConsumerSecret", 0, 0, "tweeter"); ?></p>
+<p>Consumer Secret: <?php PrintSettingText("TwitterConsumerSecret", 0, 0, 64, 64, "tweeter"); ?> <?php PrintSettingSave("Consumer Secret", "TwitterConsumerSecret", 0, 0, "tweeter"); ?></p>
 
 Doc: https://github.com/FalconChristmas/fpp/blob/d3d0f235257d23b74de8ebc19f2f17778b0dbb85/www/common.php
 
